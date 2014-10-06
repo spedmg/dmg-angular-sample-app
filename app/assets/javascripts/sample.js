abff2d970a31daceb9b2d735d447c0cf08d091a0
Chad Brown <sonyit@chad-brown-p2018883-2.spe.sony.com>
2014-10-06 16:00:15 -0700

    Updated location provider to use html5 mode

--- app/assets/javascripts/sample.js
+++ app/assets/javascripts/sample.js
@@ -3,3 +3,11 @@ var controllers = angular.module('controllers', []);
 var services = angular.module('services', []);
 
 var sample = angular.module('sample', ['ng', 'ui.router', 'controllers', 'services']);
+
+sample.config(
+  [
+    '$locationProvider',
+    function (locationProvider) {
+      locationProvider.html5Mode(true);
+    }
+  ]);
